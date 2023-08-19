37fbaff72654c59f8a9cbbf436b140a54bae7d48
Lee <94841127+galaxian@users.noreply.github.com>
2023-08-20 01:55:53 +0900

    feat : food table 초기 데이터

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into food (id, name, price, created_at, modified_at) values (1, 'pizza', 15000, null, null);
+insert into food (id, name, price, created_at, modified_at) values (2, 'chicken', 20000, null, null);
+insert into food (id, name, price, created_at, modified_at) values (3, 'hamberger', 7000, null, null);
