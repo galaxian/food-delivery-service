eeaa756d9677b2ca4b70a0418f951eb4f66ba5c3
Lee <94841127+galaxian@users.noreply.github.com>
2023-08-20 03:37:35 +0900

    feat(food) : 초기 데이터 sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into food (id, name, price, created_at, modified_at) values (1, 'pizza', 15000, null, null);
-insert into food (id, name, price, created_at, modified_at) values (2, 'chicken', 20000, null, null);
-insert into food (id, name, price, created_at, modified_at) values (3, 'hamberger', 7000, null, null);
+insert into food (name, price, created_at, modified_at) values ('pizza', 15000, null, null);
+insert into food (name, price, created_at, modified_at) values ('chicken', 20000, null, null);
+insert into food (name, price, created_at, modified_at) values ('hamberger', 7000, null, null);
