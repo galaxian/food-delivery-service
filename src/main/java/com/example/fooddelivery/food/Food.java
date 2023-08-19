b7a79cea7d49658085c77d452d6c2d3ee641704f
Lee <94841127+galaxian@users.noreply.github.com>
2023-08-20 01:49:51 +0900

    feat : food entity jpa 적용 및 수정

--- src/main/java/com/example/fooddelivery/food/Food.java
+++ src/main/java/com/example/fooddelivery/food/Food.java
@@ -1,32 +1,38 @@
 package com.example.fooddelivery.food;
 
+import com.example.fooddelivery.common.TimeStamped;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Table(name = "food")
 @Getter
-public class Food {
-    private long id;
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Food extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "price", nullable = false)
     private int price;
-    private String explain;
-    private String group;
 
-    //private List<Comment> comments;
-    //private List<Image> images;
-
-    private String createdAt;
-    private String modifiedAt;
-
-    public Food(String name, int price, String explain, String group) {
-        this.name = name;
-        this.price = price;
-        this.explain = explain;
-        this.group = group;
+    public static Food createFood(String name, int price) {
+        return new Food(name, price);
     }
 
-    public void updateFood(String name, int price, String explain, String group) {
+    private Food(String name, int price) {
         this.name = name;
         this.price = price;
-        this.explain = explain;
-        this.group = group;
+    }
+
+    public void updateFood(String name, int price) {
+        this.name = name;
+        this.price = price;
     }
 }
