08ddf3bc9f3453224ef8f7b121bc04898d9de631
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-04 22:25:41 +0900

    feat(restaurant) : RestaurantService 수정 로직 구현

--- src/main/java/com/example/fooddelivery/restaurant/service/RestaurantService.java
+++ src/main/java/com/example/fooddelivery/restaurant/service/RestaurantService.java
@@ -5,6 +5,7 @@ import com.example.fooddelivery.restaurant.domain.Restaurant;
 import com.example.fooddelivery.restaurant.dto.CreateRestaurantReqDto;
 import com.example.fooddelivery.restaurant.dto.RestaurantDetailResDto;
 import com.example.fooddelivery.restaurant.dto.RestaurantResDto;
+import com.example.fooddelivery.restaurant.dto.UpdateRestaurantReqDto;
 import com.example.fooddelivery.restaurant.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,7 @@ public class RestaurantService {
         return saveRestaurant.getId();
     }
 
+    @Transactional
     public RestaurantDetailResDto findRestaurant(Long restaurantId) {
         Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(
                 () -> new NotFoundException("식당을 찾을 수 없습니다.")
@@ -39,8 +41,17 @@ public class RestaurantService {
         return new RestaurantDetailResDto(restaurant);
     }
 
+    @Transactional
     public List<RestaurantResDto> findAllRestaurant() {
         List<Restaurant> restaurantList = restaurantRepository.findAll();
         return restaurantList.stream().map(RestaurantResDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void updateRestaurant(UpdateRestaurantReqDto reqDto, Long restaurantId) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(
+                () -> new NotFoundException("식당이 존재하지 않습니다.")
+        );
+        restaurant.update(reqDto.getName(), reqDto.getMinPrice(), reqDto.getDeliveryFee());
     }
 }
