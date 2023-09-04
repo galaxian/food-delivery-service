88bf1db3def4b9f6cdb9e51000f96d851aa6ff66
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-04 21:51:23 +0900

    feat(restaurant) : RestaurantService 상세조회 로직 구현

--- src/main/java/com/example/fooddelivery/restaurant/service/RestaurantService.java
+++ src/main/java/com/example/fooddelivery/restaurant/service/RestaurantService.java
@@ -1,7 +1,9 @@
 package com.example.fooddelivery.restaurant.service;
 
+import com.example.fooddelivery.common.exception.NotFoundException;
 import com.example.fooddelivery.restaurant.domain.Restaurant;
 import com.example.fooddelivery.restaurant.dto.CreateRestaurantReqDto;
+import com.example.fooddelivery.restaurant.dto.RestaurantDetailResDto;
 import com.example.fooddelivery.restaurant.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,5 +26,12 @@ public class RestaurantService {
         Restaurant saveRestaurant = restaurantRepository.save(restaurant);
 
         return saveRestaurant.getId();
+    }
+
+    public RestaurantDetailResDto findRestaurant(Long restaurantId) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(
+                () -> new NotFoundException("식당을 찾을 수 없습니다.")
+        );
+        return new RestaurantDetailResDto(restaurant);
     }
 }
