02c721939d66899150773a62c543825634e37cab
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-04 22:12:55 +0900

    feat(restaurant) : RestaurantController 전체 조회 api 구현

--- src/main/java/com/example/fooddelivery/restaurant/controller/RestaurantController.java
+++ src/main/java/com/example/fooddelivery/restaurant/controller/RestaurantController.java
@@ -2,12 +2,14 @@ package com.example.fooddelivery.restaurant.controller;
 
 import com.example.fooddelivery.restaurant.dto.CreateRestaurantReqDto;
 import com.example.fooddelivery.restaurant.dto.RestaurantDetailResDto;
+import com.example.fooddelivery.restaurant.dto.RestaurantResDto;
 import com.example.fooddelivery.restaurant.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/restaurants")
@@ -23,6 +25,11 @@ public class RestaurantController {
     @GetMapping("{restaurantId}")
     public ResponseEntity<RestaurantDetailResDto> findRestaurant(@PathVariable Long restaurantId) {
         return ResponseEntity.ok(restaurantService.findRestaurant(restaurantId));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<RestaurantResDto>> findAllRestaurant() {
+        return ResponseEntity.ok(restaurantService.findAllRestaurant());
     }
 
     @PostMapping("")
