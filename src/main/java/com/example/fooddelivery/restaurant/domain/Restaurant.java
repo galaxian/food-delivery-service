5b2abf0a18f075bac24ca5879ed1ed187387e646
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-04 18:35:10 +0900

    feat(restaurant) : restaurant 엔티티 연관관계 설정

--- src/main/java/com/example/fooddelivery/restaurant/domain/Restaurant.java
+++ src/main/java/com/example/fooddelivery/restaurant/domain/Restaurant.java
@@ -1,11 +1,16 @@
 package com.example.fooddelivery.restaurant.domain;
 
 import com.example.fooddelivery.common.TimeStamped;
+import com.example.fooddelivery.food.domain.Food;
+import com.example.fooddelivery.menu.domain.Menu;
+import com.example.fooddelivery.order.domain.Order;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "restaurants")
 @Getter
@@ -24,4 +29,16 @@ public class Restaurant extends TimeStamped {
 
     @Column(name = "delivery_fee")
     private int deliveryFee;
+
+    @OneToMany(mappedBy = "restaurant")
+    @JoinColumn(name = "order_id")
+    private List<Order> orders = new ArrayList<>();
+
+    @OneToMany(mappedBy = "restaurant")
+    @JoinColumn(name = "menu_id")
+    private List<Menu> menus = new ArrayList<>();
+
+    @OneToMany(mappedBy = "restaurant")
+    @JoinColumn(name = "food_id")
+    private List<Food> foods = new ArrayList<>();
 }
