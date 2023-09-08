39240feeda01ed3ccdefdc81304b7f64f74e752f
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-09 00:27:22 +0900

    test(restaurant) : 식당 생성 성공 테스트코드 작성

---
+++ src/test/java/com/example/fooddelivery/restaurant/service/RestaurantServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.fooddelivery.restaurant.service;
+
+import com.example.fooddelivery.restaurant.domain.Restaurant;
+import com.example.fooddelivery.restaurant.dto.CreateRestaurantReqDto;
+import com.example.fooddelivery.restaurant.repository.RestaurantRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class RestaurantServiceTest {
+
+    @Mock
+    private RestaurantRepository restaurantRepository;
+
+    @InjectMocks
+    private RestaurantService restaurantService;
+
+    @Test
+    void 정상적으로_식당_생성() {
+        //given
+        CreateRestaurantReqDto createRestaurantReqDto = new CreateRestaurantReqDto("ㅁㅁ식당",
+            10000, 3000);
+
+        given(restaurantRepository.save(any())).willReturn(new Restaurant(1L, createRestaurantReqDto.getName(),
+            createRestaurantReqDto.getMinPrice(), createRestaurantReqDto.getDeliveryFee(),
+            null, null, null));
+
+        //when
+        Long result = restaurantService.createRestaurant(createRestaurantReqDto);
+
+        //then
+        assertThat(result).isEqualTo(1L);
+    }
+
+}
