cbb5bdb8a7f7e91c152fa139178bfca3444cb279
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-09 02:23:17 +0900

    feat(restaurant) : 테스트 코드 성공에 필요한 코드 추가

--- src/main/java/com/example/fooddelivery/restaurant/dto/UpdateRestaurantReqDto.java
+++ src/main/java/com/example/fooddelivery/restaurant/dto/UpdateRestaurantReqDto.java
@@ -7,4 +7,10 @@ public class UpdateRestaurantReqDto {
     private String name;
     private int minPrice;
     private int deliveryFee;
+
+    public UpdateRestaurantReqDto(String name, int minPrice, int deliveryFee) {
+        this.name = name;
+        this.minPrice = minPrice;
+        this.deliveryFee = deliveryFee;
+    }
 }
