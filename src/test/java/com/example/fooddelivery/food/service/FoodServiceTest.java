865e9cfb1817f71752278cd4fc23dfc9359e93fe
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-11 00:15:21 +0900

    test(food) : 음식 생성 성공 테스트 코드 작성

---
+++ src/test/java/com/example/fooddelivery/food/service/FoodServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.fooddelivery.food.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.fooddelivery.food.domain.Food;
+import com.example.fooddelivery.food.dto.FoodRequestDto;
+import com.example.fooddelivery.food.repository.FoodRepository;
+import com.example.fooddelivery.restaurant.domain.Restaurant;
+import com.example.fooddelivery.restaurant.repository.RestaurantRepository;
+
+@ExtendWith(MockitoExtension.class)
+class FoodServiceTest {
+
+	@Mock
+	private FoodRepository foodRepository;
+
+	@Mock
+	private RestaurantRepository restaurantRepository;
+
+	@InjectMocks
+	private FoodService foodService;
+
+	@Test
+	void 정상적으로_음식_생성() {
+		//given
+		FoodRequestDto requestDto = new FoodRequestDto("음식이름", 10000);
+		Long restaurantId = 1L;
+
+		given(restaurantRepository.findById(any())).willReturn(Optional.of(new Restaurant(restaurantId, null,
+			0, 0, null, null, null)));
+		given(foodRepository.save(any())).willReturn(new Food(1L, requestDto.getName(),
+			requestDto.getPrice(), null));
+
+		//when
+		Long result = foodService.createFood(requestDto, restaurantId);
+
+		//then
+		assertThat(result).isEqualTo(1L);
+	}
+
+}
