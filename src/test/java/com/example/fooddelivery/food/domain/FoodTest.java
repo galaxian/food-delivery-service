8e135b7fbf101ba92b04b7ae6670787ca2711637
Lee <94841127+galaxian@users.noreply.github.com>
2023-09-11 01:30:12 +0900

    test(food) : 음식 가격이 음수가 아닌 경우 생성 성공 테스트 코드 작성

---
+++ src/test/java/com/example/fooddelivery/food/domain/FoodTest.java
@@ -0,0 +1,27 @@
+package com.example.fooddelivery.food.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.example.fooddelivery.restaurant.domain.Restaurant;
+
+class FoodTest {
+
+	@ParameterizedTest
+	@ValueSource(ints = {0, 10, 200, 231231})
+	void 음식의_가격이_음수가_아니면_생성_성공(int price) {
+		//given
+		String name = "음식이름";
+		Restaurant restaurant = new Restaurant(1L, "식당이름", 10000, 1000,
+			null, null, null);
+
+		//when
+		//then
+		assertDoesNotThrow(() -> Food.createFood(name, price, restaurant));
+
+	}
+
+}
