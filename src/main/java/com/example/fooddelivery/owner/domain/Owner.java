2034276cd4a41d34e85a8796e581d54c3445b389
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-09 18:11:24 +0900

    feat(owner) : owner entity 구현

---
+++ src/main/java/com/example/fooddelivery/owner/domain/Owner.java
@@ -0,0 +1,32 @@
+package com.example.fooddelivery.owner.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.example.fooddelivery.common.TimeStamped;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "owners")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Owner extends TimeStamped {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "identifiers", unique = true, nullable = false)
+	private String identifier;
+
+	@Column(name = "password", nullable = false)
+	private String encryptPassword;
+}
