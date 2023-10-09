0eddd8094ab98a7e43700fe33330117cf107b793
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-09 23:01:51 +0900

    test(auth) : 로그인 성공 테스트 코드 작성

---
+++ src/test/java/com/example/fooddelivery/auth/service/AuthServiceTest.java
@@ -0,0 +1,63 @@
+package com.example.fooddelivery.auth.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.fooddelivery.auth.dto.LoginReqDto;
+import com.example.fooddelivery.auth.dto.LoginResDto;
+import com.example.fooddelivery.common.JwtProvider;
+import com.example.fooddelivery.common.PasswordEncoder;
+import com.example.fooddelivery.owner.domain.Owner;
+import com.example.fooddelivery.owner.repository.OwnerRepository;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+	private static Owner owner = new Owner("abed1234", "xcvi0987");
+
+	@Mock
+	private OwnerRepository ownerRepository;
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+
+	@Mock
+	private JwtProvider jwtProvider;
+
+	@InjectMocks
+	private AuthService authService;
+
+	@Test
+	@DisplayName("로그인 성공")
+	void login() {
+		//given
+		LoginReqDto reqDto = new LoginReqDto("abed1234", "xcvi0987");
+		String accessToken = "accessToken";
+
+		given(ownerRepository.findByIdentifier(any()))
+			.willReturn(Optional.of(owner));
+		given(passwordEncoder.generateSalt(any()))
+			.willReturn("salt");
+		given(passwordEncoder.encrypt(any(), any()))
+			.willReturn("encrypt-password");
+		given(jwtProvider.createAccessToken(any(), any()))
+			.willReturn(accessToken);
+
+		//when
+		LoginResDto result = authService.login(reqDto);
+
+		//then
+		assertThat(result.getAccessToken()).isEqualTo(accessToken);
+
+	}
+}
