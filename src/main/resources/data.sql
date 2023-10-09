e450b942fb5f16a3517c3092f61a3c7ccafd13d5
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-09 22:45:36 +0900

    feat(auth) : 로그인 반환 dto 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-insert into food (name, price, created_at, modified_at) values ('pizza', 15000, null, null);
-insert into food (name, price, created_at, modified_at) values ('chicken', 20000, null, null);
-insert into food (name, price, created_at, modified_at) values ('hamberger', 7000, null, null);
+insert into restaurants (id, name, min_price, delivery_fee) values (1, 'food shop', 10000, 3000);
+
+insert into food (id, name, price, restaurant_id) values (2, 'pizza', 15000, 1);
+insert into food (id, name, price, restaurant_id) values (3, 'chicken', 20000, 1);
+insert into food (id, name, price, restaurant_id) values (4, 'hamberger', 7000, 1);
