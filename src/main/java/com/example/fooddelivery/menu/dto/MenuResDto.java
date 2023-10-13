ebfbe45caa0e3459042489c91d8c802ba854b79f
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-13 20:14:03 +0900

    feat(menu) : 일반용 메뉴 조회 Dto 작성

--- src/main/java/com/example/fooddelivery/menu/dto/MenuResDto.java
+++ src/main/java/com/example/fooddelivery/menu/dto/MenuResDto.java
@@ -2,23 +2,22 @@ package com.example.fooddelivery.menu.dto;
 
 import com.example.fooddelivery.menu.domain.Menu;
 import com.example.fooddelivery.menu.domain.MenuStatus;
+
 import lombok.Getter;
 
 @Getter
 public class MenuResDto {
-    private Long id;
-    private String name;
-    private int price;
-    private String describe;
-    private boolean isDisplay;
-    private MenuStatus status;
+	private Long id;
+	private String name;
+	private int price;
+	private String describe;
+	private MenuStatus status;
 
-    public MenuResDto(Menu menu) {
-        this.id = menu.getId();
-        this.name = menu.getName();
-        this.price = menu.getPrice();
-        this.describe = menu.getDescribe();
-        this.isDisplay = menu.isDisplay();
-        this.status = menu.getMenuStatus();
-    }
+	public MenuResDto(Menu menu) {
+		this.id = menu.getId();
+		this.name = menu.getName();
+		this.price = menu.getPrice();
+		this.describe = menu.getDescribe();
+		this.status = menu.getMenuStatus();
+	}
 }
