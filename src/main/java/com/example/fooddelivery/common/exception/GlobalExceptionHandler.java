1fbdaca1bbe295d2d352c0dfd8e7baea8110de67
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-17 17:03:26 +0900

    feat : GlobalExceptionHandler에 MethodArgumentNotVaildException 처리 로직 구현

--- src/main/java/com/example/fooddelivery/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/fooddelivery/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,32 @@
 package com.example.fooddelivery.common.exception;
 
+import static com.example.fooddelivery.common.exception.ErrorCode.*;
+
+import java.util.Objects;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException e,
+        HttpHeaders headers,
+        HttpStatus status,
+        WebRequest request
+    ) {
+        String message = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
+        return ResponseEntity.badRequest()
+            .body(new ErrorResponse(BAD_REQUEST, message));
+    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
