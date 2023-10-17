48386f97668da06cf5a94d36a7593a576e44c992
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-17 18:14:48 +0900

    test : FoodController 음식 조회 시 음식이 없는 경우 실패 테스트 코드 작성

--- src/test/java/com/example/fooddelivery/food/controller/FoodControllerTest.java
+++ src/test/java/com/example/fooddelivery/food/controller/FoodControllerTest.java
@@ -287,6 +287,24 @@ class FoodControllerTest extends AbstractRestDocsTest {
 			.andExpect(jsonPath("$.message").value("식당 주인만 사용할 수 있는 기능입니다."));
 	}
 
+	@Test
+	@DisplayName("음식 조회 시 음식이 없는 경우 실패")
+	void failFindFoodByNotFoundFood() throws Exception {
+		//given
+		Long restaurantId = 1L;
+		Long foodId = 1L;
+		given(foodService.findFood(anyString(), anyLong(), anyLong()))
+			.willThrow(new NotFoundException("음식이 존재하지 않습니다."));
+
+		//when
+		ResultActions resultActions = findFood(restaurantId, foodId);
+
+		//then
+		resultActions
+			.andExpect(status().isNotFound())
+			.andExpect(jsonPath("$.message").value("음식이 존재하지 않습니다."));
+	}
+
 	private ResultActions createFood(FoodRequestDto requestDto) throws Exception {
 		return mockMvc.perform(post("/api/v1/restaurants/1/foods")
 			.header(AUTHORIZATION, TOKEN_DTO.getAccessToken())
