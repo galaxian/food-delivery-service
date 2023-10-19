1186988ac8f2f8e8724e701911b9ef88794792ab
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-19 16:34:18 +0900

    docs : 식당이 존재하지 않는 경우 adoc 테스트 추가

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into restaurants (id, name, min_price, delivery_fee) values (1, 'food shop', 10000, 3000);
-
-insert into food (id, name, price, restaurant_id) values (2, 'pizza', 15000, 1);
-insert into food (id, name, price, restaurant_id) values (3, 'chicken', 20000, 1);
-insert into food (id, name, price, restaurant_id) values (4, 'hamberger', 7000, 1);
