2c37b3dd9021fac67f2a2f40ed7e113e5a60edc1
Lee <94841127+galaxian@users.noreply.github.com>
2023-10-24 16:29:06 +0900

    test : OrderController 메뉴 조회 성공 테스트 코드 작성

--- src/test/java/com/example/fooddelivery/order/controller/OrderControllerTest.java
+++ src/test/java/com/example/fooddelivery/order/controller/OrderControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.order.controller;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.http.HttpHeaders.*;
@@ -9,6 +10,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -19,13 +21,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.example.fooddelivery.auth.dto.LoginResDto;
 import com.example.fooddelivery.common.AbstractRestDocsTest;
 import com.example.fooddelivery.common.exception.NotFoundException;
+import com.example.fooddelivery.order.domain.OrderStatus;
 import com.example.fooddelivery.order.dto.CreateOrderReqDto;
 import com.example.fooddelivery.order.dto.MenuQuantityReqDto;
+import com.example.fooddelivery.order.dto.OrderDetailResDto;
+import com.example.fooddelivery.order.dto.OrderMenuResDto;
 import com.example.fooddelivery.order.service.OrderService;
 
 @WebMvcTest(OrderController.class)
@@ -131,10 +137,83 @@ class OrderControllerTest extends AbstractRestDocsTest {
 
 	}
 
+	@DisplayName("주문 조회 성공")
+	@Test
+	void successFindOrder() throws Exception {
+		//given
+		Long orderId = 1L;
+		given(orderService.findOrder(anyLong()))
+			.willReturn(new OrderDetailResDto(1L, 20000, LocalDateTime.now(), OrderStatus.WAITING,
+				new ArrayList<>(Arrays.asList(
+					new OrderMenuResDto(1L, "양념치킨", 10000, 1),
+					new OrderMenuResDto(2L, "맛있는치킨", 10000, 1)
+				))));
+
+		//when
+		ResultActions resultActions = findOrder(orderId);
+
+		//then
+		MvcResult mvcResult = resultActions
+			.andExpect(status().isOk())
+			.andDo(
+				restDocs.document(
+					responseFields(
+						fieldWithPath("id")
+							.type(JsonFieldType.NUMBER)
+							.description("주문 식별자"),
+						fieldWithPath("totalPrice")
+							.type(JsonFieldType.NUMBER)
+							.description("주문 총액"),
+						fieldWithPath("orderTime")
+							.type(JsonFieldType.STRING)
+							.description("주문 시간"),
+						fieldWithPath("orderStatus")
+							.type(JsonFieldType.VARIES)
+							.description("주문 상태"),
+						fieldWithPath("orderMenuList")
+							.type(JsonFieldType.ARRAY)
+							.description("주문 메뉴 리스트"),
+						fieldWithPath("orderMenuList[].id")
+							.type(JsonFieldType.NUMBER)
+							.description("메뉴 식별자"),
+						fieldWithPath("orderMenuList[].name")
+							.type(JsonFieldType.STRING)
+							.description("메뉴 이름"),
+						fieldWithPath("orderMenuList[].price")
+							.type(JsonFieldType.NUMBER)
+							.description("메뉴 가격"),
+						fieldWithPath("orderMenuList[].quantity")
+							.type(JsonFieldType.NUMBER)
+							.description("메뉴 갯수")
+					)
+				)
+			)
+			.andReturn();
+
+		OrderDetailResDto resDto = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(),
+			OrderDetailResDto.class
+		);
+
+		assertThat(resDto.getId()).isEqualTo(orderId);
+		assertThat(resDto.getTotalPrice()).isEqualTo(20000);
+		assertThat(resDto.getOrderStatus()).isEqualTo(OrderStatus.WAITING);
+		assertThat(resDto.getOrderMenuList().get(0).getId()).isEqualTo(1L);
+		assertThat(resDto.getOrderMenuList().get(0).getName()).isEqualTo("양념치킨");
+		assertThat(resDto.getOrderMenuList().get(0).getPrice()).isEqualTo(10000);
+		assertThat(resDto.getOrderMenuList().get(0).getQuantity()).isEqualTo(1);
+
+	}
+
 	private ResultActions createOrder(CreateOrderReqDto reqDto, Long restaurantId) throws Exception {
 		return mockMvc.perform(post("/api/v1/restaurants/{restaurantId}/orders", restaurantId)
 			.contentType(APPLICATION_JSON)
 			.content(objectMapper.writeValueAsString(reqDto)));
+	}
+
+	private ResultActions findOrder(Long orderId) throws Exception{
+		return mockMvc.perform(get("/api/v1/orders/{orderId}", orderId)
+			.contentType(APPLICATION_JSON));
 	}
 
 }
