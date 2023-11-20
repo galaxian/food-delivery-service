584b9ea8be6e0a050702b81819c499205bd4c434
Lee <94841127+galaxian@users.noreply.github.com>
2023-11-20 20:52:00 +0900

    feat : 요청 dto에 주소 입력 추가

--- src/main/java/com/example/fooddelivery/restaurant/dto/CreateRestaurantReqDto.java
+++ src/main/java/com/example/fooddelivery/restaurant/dto/CreateRestaurantReqDto.java
@@ -7,10 +7,19 @@ public class CreateRestaurantReqDto {
     private String name;
     private int minPrice;
     private int deliveryFee;
+    private String state;
+    private String city;
+    private String street;
+    private String etcAddress;
 
-    public CreateRestaurantReqDto(String name, int minPrice, int deliveryFee) {
+    public CreateRestaurantReqDto(String name, int minPrice, int deliveryFee, String state, String city,
+        String street, String etcAddress) {
         this.name = name;
         this.minPrice = minPrice;
         this.deliveryFee = deliveryFee;
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.etcAddress = etcAddress;
     }
 }
