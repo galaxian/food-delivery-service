d4ed4e3067df8902835dadf8ba5e956009b01538
Lee <94841127+galaxian@users.noreply.github.com>
2023-11-23 19:55:04 +0900

    feat : JPQL 식당 검색 쿼리 구현

--- src/main/java/com/example/fooddelivery/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/example/fooddelivery/restaurant/repository/RestaurantRepository.java
@@ -1,12 +1,24 @@
 package com.example.fooddelivery.restaurant.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.example.fooddelivery.restaurant.domain.City;
 import com.example.fooddelivery.restaurant.domain.Restaurant;
+import com.example.fooddelivery.restaurant.domain.State;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 	Optional<Restaurant> findByName(String name);
+
+	// todo 조건에 따른 분기 필요
+	@Query(value = "select distinct r from Restaurant r "
+		+ "where r.name like %:keyword% "
+		+ "and r.restaurantAddress.state = :state "
+		+ "and r.restaurantAddress.city = :city ")
+	List<Restaurant> findAllByKeywordAndAddress(String keyword, State state, City city);
 }
